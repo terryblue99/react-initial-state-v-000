086968d5c40fbedf778e7f95524c35ef56068328
terryblue99 <terryblue@comcast.net>
2018-12-14 16:33:03 -0600

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,35 @@
-// Component Code Goes Here
+import React from 'react';
+
+class Address extends React.Component {
+  render() {
+    return (
+      <div className="address">{this.props.street}, {this.props.city}</div>
+    );
+  }
+}
+
+export default Address;
+
+// While component state is a very powerful feature, it should be used as sparingly as possible. 
+// State is hard to manage and can be very easy to lose sight of. The more state we introduce 
+// in our application, the harder it will be to keep track of all of the changes in our data.
+ 
+// class Address extends React.Component {
+//   constructor(props) {
+//     super(props);
+ 
+//     this.state = {
+//       fullAddress: `${props.street}, ${props.city}`
+//     }
+//   } 
+ 
+//   render() {
+//     return (
+//       <div className="address">
+//         {this.state.fullAddress}
+//       </div>
+//     );
+//   }
+// }
+ 
+// export default Address;
--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,1 +1,25 @@
-// Component Code Goes Here
+import React from 'react';
+ 
+class ToggleButton extends React.Component { 
+  constructor() {
+    super();
+ 
+    this.state = {
+      isEnabled: false
+    }
+  }
+ 
+  render() {
+    return (
+      <button className="toggle-button">
+        I am toggled {this.state.isEnabled ? 'on' : 'off'}
+      </button>
+    )
+  }
+}
+ 
+export default ToggleButton;
+
+// While component state is a very powerful feature, it should be used as sparingly as possible. 
+// State is hard to manage and can be very easy to lose sight of. The more state we introduce 
+// in our application, the harder it will be to keep track of all of the changes in our data.
